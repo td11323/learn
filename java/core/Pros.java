840ef0df1412f001a5d402a9c9e9fb7ef6c70415
td11323 <30143520@qq.com>
2017-08-08 21:27:37 +0800

    add Properties and related code

---
+++ java/core/Pros.java
@@ -0,0 +1,28 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Properties;
+import java.io.IOException;
+
+public class Pros {
+    public static void main(String[] args) throws NumberFormatException,IOException{
+        Properties ps = new Properties();
+        ps.put("name", "me");
+        ps.put("age", "23");
+        FileOutputStream stream=new FileOutputStream("a.properties");
+        ps.store(stream,"test");
+        stream.flush();
+        stream.close();
+
+        FileInputStream inStream=new FileInputStream("a.properties");
+        ps=new Properties();
+        ps.load(inStream);
+
+        String name = ps.getProperty("name");
+        String ageString=ps.getProperty("age");
+        System.out.println(ageString);
+        int age = Integer.parseInt(ageString);
+        System.out.printf("name=%s, age=%d \r\n", name, age);
+
+        inStream.close();
+    }
+}
--- k8s/readme.md
+++ k8s/readme.md
@@ -2,5 +2,5 @@
 ## Installation
 1. Step1 [Install Docker](../docker/startup.md)
 2. Step2 [Install kubectl](kubectl.install.md)
-3. Step3 [Install kubelet](kubelet.install.md)
-4. Step4 [Initialize your master]
+3. Step3 [Install kubelet & kubeadm](kubelet.install.md)
+4. Step4 [Initialize your master](https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/)
